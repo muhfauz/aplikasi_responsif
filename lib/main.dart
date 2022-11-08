234670b1dc586787c21e9a2803fe2796998b7ccb
Sutrisno <sutrisno.stz@bsi.ac.id>
2022-11-08 09:08:13 +0700

    Ubah landscpae

--- lib/main.dart
+++ lib/main.dart
@@ -30,49 +30,81 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final MediaQueryHeigh = MediaQuery.of(context).size.height;
-    // final  myBar = AppBar(
-    //    centerTitle: true,
-    //     title: Text('Aplikasi Responsive');
+    final MediaQueryWidth = MediaQuery.of(context).size.width;
     final myApbar = AppBar(
       centerTitle: true,
       title: Text('Aplikasi Responsive'),
     );
+    final bool isLanscape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
     final myBody = MediaQueryHeigh -
         myApbar.preferredSize.height -
         MediaQuery.of(context).padding.top;
     return Scaffold(
       appBar: myApbar,
-      body: Column(
-        children: [
-          Container(
-            width: 400,
-            height: myBody * 0.4,
-            color: Colors.redAccent,
-            child: Image.network('https://picsum.photos/id/10/200/300'),
-          ),
-          Container(
-            width: 400,
-            height: myBody * 0.6,
-            color: Colors.blue,
-            child: ListView.builder(
-                itemCount: 100,
-                itemBuilder: (context, index) {
-                  return Dismissible(
-                    key: Key(index.toString()),
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        backgroundImage: NetworkImage(
-                            'https://picsum.photos/id/${index}/200/300'),
-                      ),
-                      title: Text(faker.person.name()),
-                      subtitle: Text(faker.lorem.sentence()),
-                      trailing: Text(faker.internet.userName()),
-                    ),
-                  );
-                }),
-          )
-        ],
-      ),
+      body: (isLanscape)
+          ? Column(
+              children: [
+                Container(
+                  width: MediaQueryWidth,
+                  height: myBody * 0.5,
+                  color: Colors.redAccent,
+                  child: Image.network('https://picsum.photos/id/10/200/300'),
+                ),
+                Container(
+                  width: 400,
+                  height: myBody * 0.5,
+                  color: Colors.blue,
+                  child: ListView.builder(
+                      itemCount: 100,
+                      itemBuilder: (context, index) {
+                        return Dismissible(
+                          key: Key(index.toString()),
+                          child: ListTile(
+                            leading: CircleAvatar(
+                              backgroundImage: NetworkImage(
+                                  'https://picsum.photos/id/${index}/200/300'),
+                            ),
+                            title: Text(faker.person.name()),
+                            subtitle: Text(faker.lorem.sentence()),
+                            trailing: Text(faker.internet.userName()),
+                          ),
+                        );
+                      }),
+                )
+              ],
+            )
+          : Column(
+              children: [
+                Container(
+                  width: MediaQueryWidth,
+                  height: myBody * 0.4,
+                  color: Colors.redAccent,
+                  child: Image.network('https://picsum.photos/id/10/200/300'),
+                ),
+                Container(
+                  width: 400,
+                  height: myBody * 0.6,
+                  color: Colors.blue,
+                  child: ListView.builder(
+                      itemCount: 100,
+                      itemBuilder: (context, index) {
+                        return Dismissible(
+                          key: Key(index.toString()),
+                          child: ListTile(
+                            leading: CircleAvatar(
+                              backgroundImage: NetworkImage(
+                                  'https://picsum.photos/id/${index}/200/300'),
+                            ),
+                            title: Text(faker.person.name()),
+                            subtitle: Text(faker.lorem.sentence()),
+                            trailing: Text(faker.internet.userName()),
+                          ),
+                        );
+                      }),
+                )
+              ],
+            ),
     );
   }
 }
